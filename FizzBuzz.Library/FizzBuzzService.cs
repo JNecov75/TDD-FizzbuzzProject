e78d58eccf0bb70205b6853224a9d4bff4f2741c
Jordan Necovski <JNecov75@gmail.com>
2019-04-10 21:16:21 -0400

    actually tested first red test, Assertion failed

--- FizzBuzz.Library/FizzBuzzService.cs
+++ FizzBuzz.Library/FizzBuzzService.cs
@@ -1,4 +1,10 @@
 namespace FizzBuzz.Library
 {
-    
+    public class FizzBuzzService 
+    {
+        public string Print(int value)
+        {
+            return string.Empty;
+        }
+    }
 }
