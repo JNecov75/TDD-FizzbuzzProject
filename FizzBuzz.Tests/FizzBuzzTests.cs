e7b330233c6c6ae8f260af1f8fbacbb3f023cdc4
Jordan Necovski <JNecov75@gmail.com>
2019-04-10 22:12:03 -0400

    red test - check if fizz prints for multiples of 3

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -12,5 +12,12 @@ namespace FizzBuzz.Tests
             FizzBuzzService _fizzBuzz = new FizzBuzzService();
             Assert.AreEqual("2", _fizzBuzz.Print(2));
         }
+
+        [TestMethod]
+        public void shouldPrintFizz() 
+        {
+            FizzBuzzService _fizzBuzz = new FizzBuzzService();
+            Assert.AreEqual("fizz", _fizzBuzz.Print(3));
+        }
     }
 }
