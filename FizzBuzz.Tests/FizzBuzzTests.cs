deb2c155471d31c81f3434e6b4375b922ff80a62
Jordan Necovski <JNecov75@gmail.com>
2019-04-10 21:09:07 -0400

    Updated project reference to dotnetcore2.2 from 1.1 and 1.4, and removed reference to dotnetwatcher in the .csproj to resolve an annoying warning

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -5,7 +5,12 @@ namespace FizzBuzz.Tests
 {
     [TestClass]
     public class FizzBuzzTests
-    {
-        
+    {
+        [TestMethod]
+        public void whenANumberIsEnteredItShouldBePrinted() 
+        {
+            FizzBuzzService _fizzBuzz = new FizzBuzzService();
+            Assert.AreEqual("1", _fizzBuzz.Print(1));
+        }
     }
 }
