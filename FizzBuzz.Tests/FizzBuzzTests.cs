b972a6b4e75eebd5733c55357cfe0f7587955e77
Jordan Necovski <JNecov75@gmail.com>
2019-04-10 22:18:51 -0400

    Refactored to follow DRY, pulled class init out into TestInitialize method

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -6,17 +6,22 @@ namespace FizzBuzz.Tests
     [TestClass]
     public class FizzBuzzTests
     {
+        FizzBuzzService _fizzBuzz;
+        [TestInitialize]
+        public void Init() 
+        {
+            _fizzBuzz = new FizzBuzzService();
+        }
+
         [TestMethod]
         public void shouldPrintNumberWhenNumberIsEntered() 
         {
-            FizzBuzzService _fizzBuzz = new FizzBuzzService();
             Assert.AreEqual("2", _fizzBuzz.Print(2));
         }
 
         [TestMethod]
         public void shouldPrintFizz() 
         {
-            FizzBuzzService _fizzBuzz = new FizzBuzzService();
             Assert.AreEqual("fizz", _fizzBuzz.Print(3));
             Assert.AreEqual("fizz", _fizzBuzz.Print(6));
         }
